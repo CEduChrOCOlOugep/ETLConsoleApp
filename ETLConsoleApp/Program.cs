f90d348ed8e767cac86f15bb4edb1a4bf449c30f
CEduChrOCOlOugep <145883956+CEduChrOCOlOugep@users.noreply.github.com>
2024-06-20 22:30:30 -0400

    App sett

--- ETLConsoleApp/Program.cs
+++ ETLConsoleApp/Program.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Serilog;
+using Serilog.Events;
 using ETLConsoleApp.Services;
 using ETLConsoleApp.Data;
 using Microsoft.EntityFrameworkCore;
