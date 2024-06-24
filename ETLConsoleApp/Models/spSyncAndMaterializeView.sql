f6f150e06b460b1b63b02a6a182f99d9b0c94426
CEduChrOCOlOugep <145883956+CEduChrOCOlOugep@users.noreply.github.com>
2024-06-24 14:59:51 -0400

    temp table speed up

--- ETLConsoleApp/Models/spSyncAndMaterializeView.sql
+++ ETLConsoleApp/Models/spSyncAndMaterializeView.sql
@@ -1,7 +1,7 @@
 CREATE PROCEDURE SyncAndMaterializeView
 AS
 BEGIN
-    -- Synchronize table2 with table1
+    -- Step 1: Synchronize table2 with table1
     MERGE INTO table2 AS target
     USING table1 AS source
     ON target.id = source.id AND target.ein = source.ein
@@ -11,14 +11,19 @@ BEGIN
             target.col2 = source.col2
     WHEN NOT MATCHED BY TARGET
         THEN INSERT (id, ein, col1, col2)
-        VALUES (source.id, source.ein, source.col1, source.col2)
-    WHEN NOT MATCHED BY SOURCE
-        THEN DELETE;
+        VALUES (source.id, source.ein, source.col1, source.col2);
 
-    -- Truncate the materialized view table to remove old data
-    TRUNCATE TABLE MaterializedView;
+    -- Step 2: Create a temporary table to hold the intermediate results
+    CREATE TABLE #TempInsert (
+        id INT,
+        ein INT,
+        col1 NVARCHAR(50),
+        col2 NVARCHAR(50),
+        col3 NVARCHAR(50),
+        col4 NVARCHAR(50)
+    );
 
-    -- Insert the latest data into the materialized view table
+    -- Step 3: Insert the ranked records into the temporary table
     WITH RankedRecords AS (
         SELECT
             t1.id,
@@ -31,8 +36,29 @@ BEGIN
         FROM table1 t1
         JOIN table2 t2 ON t1.id = t2.id AND t1.ein = t2.ein
     )
-    INSERT INTO MaterializedView (id, ein, col1, col2, col3, col4)
+    INSERT INTO #TempInsert (id, ein, col1, col2, col3, col4)
     SELECT id, ein, col1, col2, col3, col4
     FROM RankedRecords
     WHERE rn = 1;
+
+    -- Step 4: Merge the temporary table into the materialized view table
+    MERGE INTO MaterializedView AS target
+    USING #TempInsert AS source
+    ON target.id = source.id AND target.ein = source.ein
+    WHEN MATCHED AND (
+            target.col1 <> source.col1 OR 
+            target.col2 <> source.col2 OR
+            target.col3 <> source.col3 OR
+            target.col4 <> source.col4)
+        THEN UPDATE SET
+            target.col1 = source.col1,
+            target.col2 = source.col2,
+            target.col3 = source.col3,
+            target.col4 = source.col4
+    WHEN NOT MATCHED BY TARGET
+        THEN INSERT (id, ein, col1, col2, col3, col4)
+        VALUES (source.id, source.ein, source.col1, source.col2, source.col3, source.col4);
+
+    -- Step 5: Drop the temporary table
+    DROP TABLE #TempInsert;
 END
