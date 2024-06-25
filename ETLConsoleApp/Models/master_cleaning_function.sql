cf36bc33179717e42d944fc2028b8cf4e3584102
CEduChrOCOlOugep <145883956+CEduChrOCOlOugep@users.noreply.github.com>
2024-06-25 17:56:30 -0400

    Update master_cleaning_function.sql

--- ETLConsoleApp/Models/master_cleaning_function.sql
+++ ETLConsoleApp/Models/master_cleaning_function.sql
@@ -1,11 +1,11 @@
-CREATE FUNCTION dbo.CleanString (@input NVARCHAR(MAX))
+CREATE FUNCTION dbo.MasterCleanString (@input NVARCHAR(MAX), @type NVARCHAR(50))
 RETURNS NVARCHAR(MAX)
 AS
 BEGIN
     DECLARE @cleaned NVARCHAR(MAX)
     SET @cleaned = LOWER(@input)
-    
-    -- Remove special characters
+
+    -- General cleaning: remove special characters and trim whitespace
     SET @cleaned = REPLACE(@cleaned, '!', '')
     SET @cleaned = REPLACE(@cleaned, '@', '')
     SET @cleaned = REPLACE(@cleaned, '#', '')
@@ -33,6 +33,54 @@ BEGIN
     SET @cleaned = REPLACE(@cleaned, '/', '')
     SET @cleaned = REPLACE(@cleaned, '`', '')
     SET @cleaned = REPLACE(@cleaned, '~', '')
+    SET @cleaned = REPLACE(@cleaned, '\', '')
+    SET @cleaned = REPLACE(@cleaned, '-', '')
+    SET @cleaned = REPLACE(@cleaned, '=', '')
+    SET @cleaned = REPLACE(@cleaned, ':', '')
+    SET @cleaned = REPLACE(@cleaned, ';', '')
+    SET @cleaned = REPLACE(@cleaned, '"', '')
+    SET @cleaned = REPLACE(@cleaned, '''', '')
+    SET @cleaned = LTRIM(RTRIM(@cleaned))
+
+    -- Address specific cleaning
+    IF @type = 'ADDRESS'
+    BEGIN
+        -- Specific replacements for addresses, like removing common noise
+        SET @cleaned = REPLACE(@cleaned, 'st', 'street')
+        SET @cleaned = REPLACE(@cleaned, 'rd', 'road')
+        SET @cleaned = REPLACE(@cleaned, 'ave', 'avenue')
+        SET @cleaned = REPLACE(@cleaned, 'blvd', 'boulevard')
+        SET @cleaned = REPLACE(@cleaned, 'dr', 'drive')
+        SET @cleaned = REPLACE(@cleaned, 'ln', 'lane')
+    END
+
+    -- City specific cleaning
+    IF @type = 'CITY'
+    BEGIN
+        -- Additional rules for cities if needed
+    END
+
+    -- State specific cleaning
+    IF @type = 'STATE'
+    BEGIN
+        -- Ensure state codes are in the correct format
+        IF LEN(@cleaned) = 2 SET @cleaned = UPPER(@cleaned)
+    END
+
+    -- ZIP code specific cleaning
+    IF @type = 'ZIP5'
+    BEGIN
+        -- Ensure it's exactly 5 digits
+        IF LEN(@cleaned) = 5 SET @cleaned = @cleaned
+        ELSE SET @cleaned = NULL -- or handle invalid ZIP5s accordingly
+    END
+
+    IF @type = 'ZIP4'
+    BEGIN
+        -- Ensure ZIP+4 format
+        IF LEN(@cleaned) = 9 SET @cleaned = LEFT(@cleaned, 5) + '-' + RIGHT(@cleaned, 4)
+        ELSE SET @cleaned = NULL -- or handle invalid ZIP+4s accordingly
+    END
 
     RETURN @cleaned
 END
